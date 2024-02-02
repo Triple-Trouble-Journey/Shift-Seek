99627984c3f8b7ae617f202dfe8a463b85f1c53a
BorislavBonevAlpha <gansdhika1@gmail.com>
2024-02-02 16:17:51 +0200

    Adding a couple of car and user sections / endpoints

--- project_app/routers/car_router.py
+++ project_app/routers/car_router.py
@@ -3,21 +3,39 @@ from base_models.car_model import Car
 from config.auth import get_current_user
 from config.db_engine import engine, db_dependency, insert_query, read_query, read_query_result
 from db_models import sqlalchemy_script
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 car_router = APIRouter(prefix='/car')
 sqlalchemy_script.Base.metadata.create_all(bind=engine)
 
 
-@car_router.post('/add', status_code=status.HTTP_201_CREATED)
+@car_router.post('/add', status_code=status.HTTP_201_CREATED, tags= {'Car Section'})
+
 async def add_car(car: Car, db: db_dependency):
     insert_query(sqlalchemy_script.Car, db, car)
 
-@car_router.get('/{car_id}', status_code=status.HTTP_200_OK)
-async def all_cars(car_id: int, db: db_dependency, current_user_payload=Depends(get_current_user)):
+@car_router.get('/{car_id}', status_code=status.HTTP_200_OK, tags= {'Car Section'})
+
+async def car_by_id(car_id: int, db: db_dependency, current_user_payload=Depends(get_current_user)):
     current_property = 'car_id'
     car = read_query(sqlalchemy_script.Car, db, car_id, current_property)
     if car is None:
         raise HTTPException(status_code=404, detail='Not found!')
     return car
 
+@car_router.get("/", status_code=status.HTTP_200_OK, tags= {'Car Section'})
+async def all_cars(db: db_dependency):
 
+    car = db.query(sqlalchemy_script.Car).all()
+
+    return car
+
+@car_router.get("/brand", status_code=status.HTTP_200_OK, tags= {'Car Section'})
+
+def find_car_by_brand(brand: str, db: db_dependency):
+    current_property = 'brand'
+    car = read_query(sqlalchemy_script.Car, db, brand, current_property)
+    if car is None:
+        raise HTTPException(status_code=404, detail='Not found!')
+    return car
--- project_app/routers/user_router.py
+++ project_app/routers/user_router.py
@@ -8,18 +8,35 @@ user_router = APIRouter(prefix='/user')
 sqlalchemy_script.Base.metadata.create_all(bind=engine)
 
 
-@user_router.get('/', status_code=status.HTTP_200_OK)
-def get_user(username: str, db: db_dependency):
+@user_router.get('/name', status_code=status.HTTP_200_OK, tags= {'User Section'})
+
+def find_user_by_name(username: str, db: db_dependency):
+
     current_property = 'username'
     user = read_query(sqlalchemy_script.User, db, username, current_property)
     if user is None:
         raise HTTPException(status_code=404, detail='Not found!')
     return user
 
-@user_router.post('/', status_code=status.HTTP_201_CREATED)
-def add_user(user: UserRegiser, db: db_dependency):
-    
+@user_router.post('/', status_code=status.HTTP_201_CREATED, tags= {'User Section'})
 
+def registration(user: UserRegiser, db: db_dependency):
+    
     new_user = user_service.create_user(user, db)
 
     return new_user
+
+@user_router.get("/",status_code=status.HTTP_200_OK, tags= {'Admin Section'})
+
+async def get_all_users(db: db_dependency):
+    users = db.query(sqlalchemy_script.User).all()
+    return users
+
+@user_router.get('/{user_id}', status_code=status.HTTP_200_OK, tags= {'User Section'})
+
+async def user_by_id(user_id: int, db: db_dependency):
+    current_property = 'user_id'
+    user = read_query(sqlalchemy_script.User, db, user_id, current_property)
+    if user is None:
+        raise HTTPException(status_code=404, detail='Not found!')
+    return user
